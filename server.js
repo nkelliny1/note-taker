5c37ebc6d99f698615515cb90a774ec184511ebe
nkelliny1 <nkelliny@outlook.com>
2020-03-07 10:54:15 -0500

    Change to serve the html pages and load js and css

--- server.js
+++ server.js
@@ -2,11 +2,12 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
+var fs = require("fs");
 
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = 7000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -14,20 +15,16 @@ app.use(express.json());
 
 
 // =============================================================
-var notes = [
-  {
-    title: "Hello",
-    text: "Hello"
-  }
-];
+let rawdata = fs.readFileSync('./db/db.json');
+let notes = JSON.parse(rawdata);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 // Routes
 // =============================================================
 
 // Basic route that sends the user first to the AJAX Page
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
-});
 
 app.get("/notes", function(req, res) {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
@@ -54,24 +51,26 @@ app.get("/api/notes/:note", function(req, res) {
 });
 
 // Create New Characters - takes in JSON input
-// app.post("/api/notes", function(req, res) {
-//   // req.body hosts is equal to the JSON post sent from the user
-//   // This works because of our body parsing middleware
-//   var newCharacter = req.body;
+app.post("/api/notes", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  var newNote = req.body;
 
-//   // Using a RegEx Pattern to remove spaces from newCharacter
-//   // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-//   newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
+  // Using a RegEx Pattern to remove spaces from newCharacter
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
 
-//   console.log(newCharacter);
+  console.log(newNote);
 
-//   characters.push(newCharacter);
+  notes.push(newNote);
 
-//   res.json(newCharacter);
-// });
+  res.json(newNote);
+});
 
 // Starts the server to begin listening
 // =============================================================
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+  });
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
